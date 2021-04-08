9e0498f81cb5f58c218c7c317c8abe9d493451eb
MARTY Morgan <morgan.marty@etud.univ-jfc.fr>
2021-04-08 22:53:38 +0200

    get world

--- src/app/restservice.service.ts
+++ src/app/restservice.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { World, Pallier, Product } from './world';
 
 
@@ -30,8 +30,14 @@ export class RestserviceService {
     return Promise.reject(error.message || error);
   }
   getWorld(): Promise<World> {
-    return this.http.get(this.server + "adventureisis/generic/world")
-      .toPromise().catch(this.handleError);
-  };
+    return this.http.get(this.server + "adventureisis/generic/world", { 
+   headers: this.setHeaders(this.user)})
+    .toPromise().catch(this.handleError);
+   };
+   
+  private setHeaders(user: string): HttpHeaders {
+    var headers = new HttpHeaders({ 'X-User': user});
+    return headers;
+    };
 
 }
